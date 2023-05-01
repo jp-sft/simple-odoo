5fc76e58452faf89ecb806d42d981ba241f3dd01
jp-sft <kvnjessy@gmail.com>
2023-05-01 12:53:26 +0100

    Intégration des changement de Smartpay

--- custom_auth_signup/__manifest__.py
+++ custom_auth_signup/__manifest__.py
@@ -4,7 +4,7 @@
     'description': "For more information, please see the documentation "
                    "https://github.com/jp-sft/simple-odoo/blob/13.0/README.md",
     'version': '1.0',
-    'author': 'Simple Odoo',
+    'author': 'Simple',
     'category': 'Tools',
     'depends': ['auth_signup'],
     'data': ["templates/two_factor_auth.xml"],
