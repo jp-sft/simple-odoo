3cbe42ae023485532b2d6ef7273f2caaa4a68fe4
Jessy Pango <kvnjessy@gmail.com>
2024-10-01 08:39:02 +0100

    Update leave_am_pm_management

--- leave_am_pm_management/models/hr_leave.py
+++ leave_am_pm_management/models/hr_leave.py
@@ -1,51 +1,108 @@
+import logging
 from odoo import models, fields, api
-from datetime import datetime
+from datetime import datetime, time
+
+_logger = logging.getLogger(__name__)
 
 
 class HrLeave(models.Model):
     _inherit = "hr.leave"
 
-    request_date_from_am_pm = fields.Selection(
-        [
-            ("am", "AM"),
-            ("pm", "PM"),
-        ],
-        string="Start AM/PM",
+    date_from = fields.Datetime(
+        "Start Date",
+        readonly=True,
+        index=True,
+        copy=False,
+        required=True,
+        default=fields.Datetime.now,
+        states={"draft": [("readonly", True)], "confirm": [("readonly", False)]},
+        tracking=True,
+        store=True,
+    )
+    date_to = fields.Datetime(
+        "End Date",
+        readonly=True,
+        copy=False,
+        required=True,
+        default=fields.Datetime.now,
+        states={"draft": [("readonly", True)], "confirm": [("readonly", False)]},
+        tracking=True,
+        store=True,
+    )
+    request_date_from_period = fields.Selection(
+        [("am", "AM"), ("pm", "PM")],
+        string="Date Period Start",
         default="am",
     )
-
-    request_date_to_am_pm = fields.Selection(
-        [
-            ("am", "AM"),
-            ("pm", "PM"),
-        ],
-        string="End AM/PM",
+    request_date_to_period = fields.Selection(
+        [("am", "AM"), ("pm", "PM")],
+        string="Date Period To",
         default="pm",
     )
+    hour_from = fields.Selection(
+        [(str(x), str(x)) for x in range(12)],
+        string="Hour From",
+        default="0",
+    )
+    hour_to = fields.Selection(
+        [(str(x), str(x)) for x in range(12)],
+        string="Hour To",
+        default="11",
+    )
 
-    @api.onchange("request_date_from_am_pm", "request_date_to_am_pm")
-    def _onchange_am_pm(self):
-        # Mettre à jour date_from et date_to selon AM ou PM
-        if self.request_date_from and self.request_date_from_am_pm:
-            self.date_from = self._adjust_date_am_pm(
-                self.request_date_from, self.request_date_from_am_pm
-            )
-        if self.request_date_to and self.request_date_to_am_pm:
-            self.date_to = self._adjust_date_am_pm(
-                self.request_date_to, self.request_date_to_am_pm
-            )
+    @api.onchange(
+        "request_date_from_period",
+        "request_date_to_period",
+        # "request_hour_from",
+        # "request_hour_to",
+        "request_date_from",
+        "request_date_to",
+        "employee_id",
+        "hour_to",
+        "hour_from",
+    )
+    def _onchange_request_parameters(self):
+        if self.hour_from and self.hour_to:
+            # Convert request_date_from and request_date_to to datetime objects
+            date_from = datetime.combine(self.request_date_from, time(0, 0))
+            date_to = datetime.combine(self.request_date_to, time(0, 0))
 
-    def _adjust_date_am_pm(self, date, am_pm):
-        if isinstance(date, datetime):
-            # Si la date est déjà un datetime, nous pouvons ajuster l'heure
-            if am_pm == "am":
-                return date.replace(hour=8, minute=0)  # Exemple d'heure pour AM
-            else:
-                return date.replace(hour=13, minute=0)  # Exemple d'heure pour PM
+            # Adjust the start time of request_date_from with request_hour_from
+            hour_from = int(self.hour_from)
+            if self.request_date_from_period == "pm" and hour_from < 12:
+                hour_from += 12
+            request_hour_from = str(hour_from)
+
+            # Fetch start work hour if needed
+            start_work_hour = 6
+            date_from = date_from.replace(hour=max(int(start_work_hour), hour_from))
+
+            # Adjust the end time of request_date_to with request_hour_to
+            hour_to = int(self.hour_to)
+            if self.request_date_to_period == "pm" and hour_to < 12:
+                hour_to += 12
+            request_hour_to = str(hour_to)
+
+            # Fetch end work hour if needed
+            end_work_hour = 17
+            date_to = date_to.replace(hour=min(int(end_work_hour), hour_to))
+
+            self.update(
+                {
+                    "date_from": date_from,
+                    "date_to": date_to,
+                    "request_hour_from": request_hour_from,
+                    "request_hour_to": request_hour_to,
+                }
+            )
         else:
-            # Si la date est un objet 'date', nous la convertissons en 'datetime'
-            date_dt = datetime.combine(date, datetime.min.time())
-            if am_pm == "am":
-                return date_dt.replace(hour=8, minute=0)  # AM
-            else:
-                return date_dt.replace(hour=13, minute=0)  # PM
+            date_from = self.request_date_from
+            date_to = self.request_date_to
+            self.update(
+                {
+                    "date_from": date_from,
+                    "date_to": date_to,
+                }
+            )
+
+        self._onchange_leave_dates()
